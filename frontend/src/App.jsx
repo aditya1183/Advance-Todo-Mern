ab535279812797721d98c404dbe603d9bd6efa41
aditya1183 <adityasharma44159@gmail.com>
2025-03-04 10:01:13 +0530

    add files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { RouterProvider, Routes, Route, BrowserRouter } from "react-router-dom";
 import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
